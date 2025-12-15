100a5c068987b8a5c674aac77275f3c3b4ffde74
Nqtural <robin.stalstad@proton.me>
2025-12-15 11:28:36 +0100

    playtest: change default gamemode to 2v2

--- protocol/src/constants.rs
+++ protocol/src/constants.rs
@@ -1,4 +1,4 @@
-pub const TEAM_SIZE: usize = 1;
+pub const TEAM_SIZE: usize = 2;
 
 pub const TEAM_ONE_START_POS: [f32; 2] = [820.0, 470.0];
 pub const TEAM_TWO_START_POS: [f32; 2] = [1160.0, 470.0];
