64b95b52363d75a2f46e2b76a8d20bac11cc1bb7
Nqtural <robin.stalstad@proton.me>
2025-12-27 20:32:18 +0100

    feat(display): condense player names to 3 characters

--- protocol/src/utils.rs
+++ protocol/src/utils.rs
@@ -6,6 +6,19 @@ use std::collections::HashSet;
 use std::net::SocketAddr;
 use crate::net_server::ServerMessage;
 
+pub fn condense_name(mut name: &str) -> String {
+    // Skip leading "The"
+    if name.len() >= 3 && name[..3].eq_ignore_ascii_case("the") {
+        name = &name[3..];
+    }
+
+    name.chars()
+        .filter(|c| c.is_ascii_alphanumeric()) // excludes spaces automatically
+        .take(3)
+        .map(|c| c.to_ascii_uppercase())
+        .collect()
+}
+
 pub async fn send_to(addr: SocketAddr, msg: ServerMessage, socket: &UdpSocket, cfg: &config::Configuration) {
     if let Ok(data) = encode_to_vec(&msg, *cfg) {
         let _ = socket.send_to(&data, addr).await;
